662d617478b7921938788ec1bd4ce0b69eb9c47a
GDPM05 <j.p.f.a009@gmail.com>
2024-12-04 15:05:07 +0000

    First Commit

---
+++ src/main/java/com/gdpm/fintrack/FinTrack.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gdpm.fintrack;
+
+/**
+ *
+ * @author gdpm
+ */
+public class FinTrack {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
